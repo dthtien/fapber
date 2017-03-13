61b2b0b80e466454edbc4b58115c564ff5320fab
The Tiến <tiendt2311@gmail.com>
2017-03-13 10:10:51 +0700

    Merge from fongfan999/faber

--- main.rb
+++ main.rb
@@ -1,1 +1,10 @@
 require './config'
+require 'json'
+
+JSON.parse(File.read('dummy/1.json')).each do | mail|
+Fapber::Page.new(
+'https://m.facebook.com/pg/foxfizz/',
+    mail,
+    mail
+  ).review
+end
